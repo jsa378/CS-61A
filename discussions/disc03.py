a962eb0597062a42ac562061461bce567da6c215
Jesse Schneider <jsa378@student.ubc.ca>
2024-05-04 18:55:39 -0700

    disc03

--- discussions/disc03.py
+++ discussions/disc03.py
@@ -32,3 +32,99 @@ def skip_factorial(n):
         return n
     else:
         return n * skip_factorial(n - 2)
+
+def is_prime(n):
+    """Returns True if n is a prime number and False otherwise.
+    >>> is_prime(2)
+    True
+    >>> is_prime(16)
+    False
+    >>> is_prime(521)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 2:
+        return True
+    def does_divide(i):
+        if n % i == 0:
+            return False
+        if i == 2:
+            return True
+        return does_divide(i - 1)
+    return does_divide(n - 1)
+
+# print(is_prime(4))
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, 
+    and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    >>> b = hailstone(1)
+    1
+    >>> b
+    1
+    """
+    print(n)
+    if n % 2 == 0:
+        return even(n)
+    else:
+        return odd(n)
+
+def even(n):
+    return 1 + hailstone(n // 2)
+
+def odd(n):
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return 1
+    return 1 + hailstone(3 * n + 1)
+
+# hailstone(10)
+
+def sevens(n, k):
+    """Return the (clockwise) position of who says n among k players.
+
+    >>> sevens(2, 5)
+    2
+    >>> sevens(6, 5)
+    1
+    >>> sevens(7, 5)
+    2
+    >>> sevens(8, 5)
+    1
+    >>> sevens(9, 5)
+    5
+    >>> sevens(18, 5)
+    2
+    """
+    def f(i, who, direction):
+        if i == n:
+            return who
+        "*** YOUR CODE HERE ***"
+        if i % 7 == 0 or has_seven(i):
+            direction *= (-1)
+        next_who = who + direction
+        if next_who == k + 1:
+            next_who = 1
+        if next_who == 0:
+            next_who = k
+        return f(i + 1, next_who, direction)  
+    return f(1, 1, 1)
+
+def has_seven(n):
+    if n == 0:
+        return False
+    elif n % 10 == 7:
+        return True
+    else:
+        return has_seven(n // 10)
+
