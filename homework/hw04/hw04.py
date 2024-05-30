20a7fb2995484a7015e6a0061ef695fb0e6a69ee
Jesse Schneider <jsa378@student.ubc.ca>
2024-05-29 19:13:18 -0700

    hw04

--- homework/hw04/hw04.py
+++ homework/hw04/hw04.py
@@ -234,3 +234,66 @@ def copy_tree(t):
     """
     return tree(label(t), [copy_tree(b) for b in branches(t)])
 
+def exp_tree(values):
+    """
+    Returns the exponential tree that can be made from VALUES
+    with the greatest possible root label
+    >>> print_tree(exp_tree([5]))
+    5
+    >>> print_tree(exp_tree([3, 2]))
+    9
+      3
+      2
+    >>> print_tree(exp_tree([2, 3, 2]))
+    512
+      2
+      9
+        3
+        2
+    >>> lst = [3, 1, 2, 3]
+    >>> print_tree(exp_tree(lst))
+    6561
+      3
+        3
+        1
+      8
+        2
+        3
+    """
+    if len(values) == 1:
+        return tree(values[0])
+    else:
+        def tree_at_split(i):
+            base = exp_tree(values[:i])
+            exponent = exp_tree(values[i:])
+            return tree(label(base) ** label(exponent), [base, exponent])
+        trees = [tree_at_split(i) for i in range(1, len(values))]
+        return max(trees, key=label)
+
+def max_path(t, k):
+    """ Return a list of the labels on any path in tree t of length at most k with the greatest sum
+    >>> t1 = tree(6, [tree(3, [tree(8)]), tree(1, [tree(9), tree(3)])])
+    >>> max_path(t1, 3)
+    [6, 3, 8]
+    >>> max_path(t1, 2)
+    [3, 8]
+    >>> t2 = tree(5, [t1, tree(7)])
+    >>> max_path(t2, 1)
+    [9]
+    >>> max_path(t2, 2)
+    [5, 7]
+    >>> max_path(t2, 3)
+    [6, 3, 8]
+    """
+    def helper(t, k, on_path):
+        if k == 0:
+            return []
+        elif is_leaf(t):
+            return [label(t)]
+        a = [ [label(t)] + helper(b, k - 1, True) for b in branches(t) ]
+        if on_path:
+            return max(a, key = sum)
+        else:
+            b = [ helper(b, k, False) for b in branches(t) ]
+            return max(a + b, key = sum)
+    return helper(t, k, False)
